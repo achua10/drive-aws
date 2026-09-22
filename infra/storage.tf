d935ff26fff089b7092af76982cf047148dbb66a
as3337@srmist.edu.in <160508820+achua10@users.noreply.github.com>
2026-09-23 00:36:32 +0530

    Add ECR Repository

--- infra/storage.tf
+++ infra/storage.tf
@@ -34,3 +34,34 @@ resource "aws_dynamodb_table" "files" {
     type = "S"
   }
 }
+
+resource "aws_ecr_repository" "app" {
+  name                 = "${local.name_prefix}-app"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only the last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
